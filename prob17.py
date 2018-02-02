c9d521527a7ee7ad9f65cf0987b02a9e416d3ccd
Canta Tronic <tokareva@jinr.ru>
2018-02-03 00:16:42 +0300

    Finish with number names from 1 to 1000 generator

--- prob17.py
+++ prob17.py
@@ -10,15 +10,24 @@ def nameGen(num):
             16: 'sixteen', 17:'seventeen', 18:'eighteen', 19:'nineteen', 20:'twenty',
             30: 'thirty', 40: 'forty', 50: 'fifty', 60: 'sixty', 70: 'seventy', 80: 'eighty',
             90: 'ninety', 100: 'hundred', 1000: 'one thousand'}
+  #1-10, 100, 1000
   if (num in my_dict):
-    return my_dict[num]
+      return my_dict[num]
+  #11-100
   if (num<100):
-    fisrtDig = 10*(num/10)
-    secDig = num - fisrtDig
-    numName = my_dict[fisrtDig]+'-'+my_dict[secDig]
-    return numName
+      fisrtDig = 10*(num/10)
+      secDig = num - fisrtDig
+      numName = my_dict[fisrtDig]+'-'+my_dict[secDig]
+      return numName
+  #101-1000
+  if (num>100) and (num <1000):
+      fisrtDig = num/100
+      secDig = num - fisrtDig*100
+      numName = my_dict[fisrtDig]+" "+my_dict[100]
+      if (secDig != 0):
+        numName = numName+ " " + nameGen(secDig)
+      return numName
 
-  
 
 def cleanStr(line):
   #return lenght of a clean string without spaces and dash
@@ -38,17 +47,8 @@ def test():
   pass
 
 def main():
-    #my_dict = {1: 'one', 2: 'two', 3: 'three', 4:'four', 5:'five', 
-               #6: 'six', 7:'seven', 8: 'eight', 9: 'nine', 10: 'ten',
-               #11: 'eleven', 12: 'twelve', 13: 'thirteen', 14:'fourteen', 15:'fifteen',
-               #16: 'sixteen', 17:'seventeen', 18:'eighteen', 19:'nineteen', 20:'twenty'}
-    for i in range(1,101):
+    for i in range(1, 1001):
         print(nameGen(i))
-    #print(nameGen(19))
-    #print(nameGen(29))
-    #print(nameGen(63))
-    #print(nameGen(14))
-    #print(nameGen(88))
     sys.exit(0)
 
 if __name__ == "__main__":
