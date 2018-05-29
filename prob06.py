656e842c49f02db1dfc76fb95aa79bfb6938bbba
Canta Tronic <tokareva@jinr.ru>
2018-05-29 12:36:57 +0300

    Renaming and refactoring ex. 6

--- prob06.py
+++ prob06.py
@@ -1,12 +1,20 @@
+#!/bin/python
 
+'''
+Sum square difference
+
+link: https://projecteuler.net/problem=6
+'''
+
+from __future__ import print_function
 import math
+import sys
 
 def sum_of_sqr(num):
   my_sum = 0
   for i in range(num+1):
     my_sum += math.pow(i,2)
   return my_sum
-
 
 def sqr_of_sum(num):
   my_sum = 0
@@ -17,6 +25,6 @@ def sqr_of_sum(num):
 def get_diff(num):
   return abs(sqr_of_sum(num) - sum_of_sqr(num))
 
-#print sum_of_sqr(10)
-#print sqr_of_sum(10)
-print "diff = ", get_diff(100)
+if __name__ == "__main__":
+  print ("Answer: ", int(get_diff(100)))
+  sys.exit(0)
